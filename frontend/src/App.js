02ee8c3b17965ecf3566c86219ebf3a0b2529ce3
eli-shallev <eli.shallev50@gmail.com>
2023-05-21 12:06:55 +0300

    initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { Home } from "./views/home";
+
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Home/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/views/home.jsx
@@ -0,0 +1,10 @@
+
+export function Home(){
+
+
+    return(
+        <div className="Home">
+            <h1>Hii</h1>
+        </div>
+    )
+}
