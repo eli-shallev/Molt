fb90f0c132eddddf0e5cddf180650249b4ec6d13
eli-shallev <eli.shallev50@gmail.com>
2023-05-23 21:02:31 +0300

    loader added

---
+++ frontend/src/cmps/loader.jsx
@@ -0,0 +1,44 @@
+export function Loader() {
+
+    return (
+        <div className="loader">
+            <svg
+                width="200"
+                height="200"
+                viewBox="0 0 200 200"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <defs>
+                    <linearGradient id="spinner-secondHalf">
+                        <stop offset="0%" stopOpacity="0" stopColor="currentColor" />
+                        <stop offset="100%" stopOpacity="0.5" stopColor="currentColor" />
+                    </linearGradient>
+                    <linearGradient id="spinner-firstHalf">
+                        <stop offset="0%" stopOpacity="1" stopColor="currentColor" />
+                        <stop offset="100%" stopOpacity="0.5" stopColor="currentColor" />
+                    </linearGradient>
+                </defs>
+
+                <g strokeWidth="8">
+                    <path stroke="url(#spinner-secondHalf)" d="M 4 100 A 96 96 0 0 1 196 100" />
+                    <path stroke="url(#spinner-firstHalf)" d="M 196 100 A 96 96 0 0 1 4 100" />
+                    <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        d="M 4 100 A 96 96 0 0 1 4 98"
+                    />
+                </g>
+
+                <animateTransform
+                    from="0 0 0"
+                    to="360 0 0"
+                    attributeName="transform"
+                    type="rotate"
+                    repeatCount="indefinite"
+                    dur="1300ms"
+                />
+            </svg>
+        </div>
+    )
+}
