35e2f4f7ec7b04079e04112ba2fd4fe104e1a45b
eli-shallev <eli.shallev50@gmail.com>
2023-05-23 18:43:38 +0300

    react router and nav bar for mobile added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,30 @@
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
 import { Discovery } from './views/discovery';
 import { HeaderDesktop } from './cmps/header-desktop';
+import { NavBar } from './cmps/nav-bar';
+import { Restaurants } from './views/restaurants';
 
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="main-layout app">
-        <HeaderDesktop/>
-        <main className='main-layout'>
-            <Discovery/>
-        </main>
-      </div>
+      <Router>
+        <div className="main-layout app">
+          <HeaderDesktop />
+          <NavBar />
+
+          <main className='main-layout'>
+            <Routes>
+              <Route path="/" element={<Discovery />} />
+              <Route path="/discovery" element={<Discovery />} />
+              <Route path="/restaurants" element={<Restaurants />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
     </Provider>
   );
 }
---
+++ frontend/src/views/restaurants.jsx
@@ -0,0 +1,9 @@
+
+export function Restaurants(){
+
+    return(
+        <div className="restaurants">
+            restaurants
+        </div>
+    )
+}
