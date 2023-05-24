08170c0079f990bddb89ddfff13a22053a8c9e3d
eli-shallev <eli.shallev50@gmail.com>
2023-05-24 20:49:40 +0300

    sass styling

--- frontend/src/cmps/header-desktop.jsx
+++ frontend/src/cmps/header-desktop.jsx
@@ -22,41 +22,43 @@ export function HeaderDesktop() {
     }, [window.scrollY])
 
     return (
-        <header className=" main-layout full header-desktop">
-            <div className="header-left">
-                <img src={require("../assets/images/molt-b&w-logo.png")} alt="Molt logo" className="header-logo" />
-                <div className={scrollY > 100 ? 'header-delivery scrolled' : 'header-delivery'}>
-                    <div className="header-adderss-label">
-                        <VscHome />
+        <div className="header-desktop-container main-layout full">
+            <header className="header-desktop">
+                <div className="header-left">
+                    <img src={require("../assets/images/molt-b&w-logo.png")} alt="Molt logo" className="header-logo" />
+                    <div className={scrollY > 100 ? 'header-delivery scrolled' : 'header-delivery'}>
+                        <div className="header-adderss-label">
+                            <VscHome />
+                        </div>
+                        <div className="header-adderss-info">
+                            <span className='header-address-const'>Delivery to</span>
+                            <span className='header-adderss-title'>Home (סמטת היין 9)</span>
+                        </div>
+                        <div className="header-address-btn">
+                            <BiChevronDown />
+                        </div>
                     </div>
-                    <div className="header-adderss-info">
-                        <span className='header-address-const'>Delivery to</span>
-                        <span className='header-adderss-title'>Home (סמטת היין 9)</span>
+                </div>
+
+                <div className="header-middle">
+                    <div className="header-search">
+                        <FiSearch />
+                        <input
+                            type="text"
+                            placeholder='Search in Molt...' />
                     </div>
-                    <div className="header-address-btn">
+                </div>
+
+                <div className="header-right">
+                    <div className="header-user-btn">
+                        <div className="header-user-img-container">
+                            <img src="https://api.multiavatar.com/user.png" alt="" />
+                        </div>
                         <BiChevronDown />
                     </div>
                 </div>
-            </div>
-
-            <div className="header-middle">
-                <div className="header-search">
-                    <FiSearch />
-                    <input
-                        type="text"
-                        placeholder='Search in Molt...' />
-                </div>
-            </div>
-
-            <div className="header-right">
-                <div className="header-user-btn">
-                    <div className="header-user-img-container">
-                        <img src="https://api.multiavatar.com/user.png" alt="" />
-                    </div>
-                    <BiChevronDown />
-                </div>
-            </div>
-        </header>
+            </header>
+        </div>
 
     )
 }
